7f2deabb35b735a2c3cd089f5a5e5aac4e60d179
memothelemo <dev@memothelemo.xyz>
2026-03-25 23:39:40 +0800

    Bump Minecraft version (`26.1.0` is stabilized)

--- src/main/java/xyz/memothelemo/instantrestock/InstantRestock.java
+++ src/main/java/xyz/memothelemo/instantrestock/InstantRestock.java
@@ -1,5 +1,6 @@
 package xyz.memothelemo.instantrestock;
 
+import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
@@ -9,12 +10,8 @@ public class InstantRestock implements DedicatedServerModInitializer {
     public static final String MOD_ID = "InstantRestock";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // As of writing this, fabric-permissions-api doesn't have the version provided
-    // for the 26.1 snapshot. Therefore, all players can have instant restock effect.
-    //
-    // TODO: Require `instantrestock` permission for this function to return true
     public static boolean canApplyIRForPlayer(ServerPlayer player) {
-        return true;
+        return Permissions.check(player, "dystopia.instantrestock");
     }
 
     @Override
